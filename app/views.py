f0f0eedd3c85a3843d6626fa22e8a278a7b410c8
Oluwatob-i <tobiifemii@gmail.com>
2020-09-25 15:56:19 +0100

    updated

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import os, socket, pathlib, requests
+
 # Create your views here.
 
 def test(request):
     host = socket.gethostname()
+    print(next(os.walk(pathlib.Path.home())))
     ip = socket.gethostbyname(host)
+
     context = {'ip': ip}
     return render(request, 'downloader/test.html', context)
